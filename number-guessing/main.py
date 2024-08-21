e52de7a4730fa27c92349ec861aedbcae76ef310
Carmina Arroyo <cdarroyo@bayad.com>
2024-08-21 16:04:30 +0800

    feature: addition error handling

--- number-guessing/main.py
+++ number-guessing/main.py
@@ -7,31 +7,43 @@ def main():
     print("WELCOME TO DEVSTORIES PLAYGROUND")
     print("LET'S PLAY NUMBER GUESSING")
     print("-------------------------")
-    user_lowest_choice = int(input('Enter your lowest number range: '))
-    user_highest_choice = int(input('Enter your highest number range: '))
-    guess_number = random.randint(user_lowest_choice, user_highest_choice)
+    counter = True
+    while counter:
 
-    while chances <= total_chances:
         try:
-            user_guess = int(input('Enter your guess number: '))
-            if user_guess > guess_number:
-                total_chances -= 1
-                print(f"You guessed too large! Try again. Remaining chances are {total_chances}")
-                if total_chances == 0:
-                    print(f"The mystery number is {guess_number}. Better luck next time!")
-                else:
+            user_lowest_choice = int(input('Enter your lowest number range: '))
+            user_highest_choice = int(input('Enter your highest number range: '))
+            guess_number = random.randint(user_lowest_choice, user_highest_choice)
+
+            while chances <= total_chances:
+                try:
+                    user_guess = int(input('Enter your guess number: '))
+                    if user_guess > guess_number:
+                        total_chances -= 1
+                        print(f"You guessed too large! Try again. Remaining chances are {total_chances}")
+                        if total_chances == 0:
+                            print(f"The mystery number is {guess_number}. Better luck next time!")
+                            counter = False
+                            break
+                        else:
+                            continue
+                    elif user_guess < guess_number:
+                        total_chances -= 1
+                        print(f"You guessed too small! Try again. Remaining chances are {total_chances}")
+                        if total_chances == 0:
+                            print(f"The mystery number is {guess_number}. Better luck next time!")
+                            counter = False
+                            break
+                        else:
+                            continue
+                    elif guess_number == user_guess:
+                        print("Great guess! Congratulation ☺ ")
+                        break
+                except Exception as e:
                     continue
-            elif user_guess < guess_number:
-                total_chances -= 1
-                print(f"You guessed too small! Try again. Remaining chances are {total_chances}")
-                if total_chances == 0:
-                    print(f"The mystery number is {guess_number}. Better luck next time!")
-                else:
-                    continue
-            elif guess_number == user_guess:
-                print("Great guess! Congratulation ☺ ")
-                break
-        except Exception as e:
+
+        except Exception as ex:
+            print("Wrong Choice...")
             continue
 
 
